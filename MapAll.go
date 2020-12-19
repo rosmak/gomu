6ad152706c60fe5526f1eb7c14988e806ce5cc63
Karlo <karlo@travelraysoftware.com>
2020-12-19 13:52:40 +0100

    MapAll func added, that accepts struct or slice

---
+++ MapAll.go
@@ -0,0 +1,23 @@
+package gomu
+
+import (
+	"reflect"
+)
+
+//MapAll ...
+func MapAll(obj interface{}) error {
+	//typeOfObj should be "struct" or "slice"
+	typeOfObj := (reflect.ValueOf(reflect.Indirect(reflect.ValueOf(obj)).Interface()).Kind()).String()
+	switch typeOfObj {
+	case "struct":
+		MapAllFromStruct(obj)
+		break
+	case "slice":
+		refSliceVal := reflect.Indirect(reflect.ValueOf(obj))
+		for i := 0; i < refSliceVal.Len(); i++ {
+			eachElem := refSliceVal.Index(i).Addr().Interface()
+			MapAllFromStruct(eachElem)
+		}
+	}
+	return nil
+}
